49b413a7813a91edf286b41ec14e5c4207d0fc56
Rounak Kumar Singh <rounak.official.2@gmail.com>
2025-09-29 01:24:54 +0530

    help and exit

--- main.go
+++ main.go
@@ -1,12 +1,82 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"os"
+	"sort"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return ErrExit
+}
+
+var ErrExit = errors.New("exit")
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	// stable order
+	keys := make([]string, 0, len(commands))
+	for k := range commands {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		c := commands[k]
+		fmt.Printf("%s: %s\n", c.name, c.description)
+	}
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		s.Scan()
+		words := s.Text()
+		switch words {
+		case "exit":
+			if err := commands["exit"].callback(); err != ErrExit {
+				panic(err)
+			}
+		case "help":
+			if err := commandHelp(); err != nil {
+				panic(err)
+			}
+		}
+	}
 }
 
 func cleanInput(text string) []string {
